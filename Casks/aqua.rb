0742194eb05a461dad9ecdca9a93c8cc459edd43
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2025-10-25 22:50:29 +0900

    Brew formula update for aqua version v2.55.1

--- Casks/aqua.rb
+++ Casks/aqua.rb
@@ -4,7 +4,7 @@ cask "aqua" do
   desc "Declarative CLI Version manager. Support Lazy Install and Sharable configuration mechanism named Registry. Switch versions seamlessly
 "
   homepage "https://github.com/aquaproj/aqua"
-  version "2.55.0"
+  version "2.55.1"
 
   livecheck do
     skip "Auto-generated on release."
@@ -15,22 +15,22 @@ cask "aqua" do
   on_macos do
     on_intel do
       url "https://github.com/aquaproj/aqua/releases/download/v#{version}/aqua_darwin_amd64.tar.gz"
-      sha256 "931c910b6543a5fdcdfaf5f1bbb68f96ab8d8498ddf1e50a1847248f2955d690"
+      sha256 "814bd2ba3b1db409e89eae126ad280413e4edfefe91f598ce173c3b21ba56ca8"
     end
     on_arm do
       url "https://github.com/aquaproj/aqua/releases/download/v#{version}/aqua_darwin_arm64.tar.gz"
-      sha256 "4797caf2d0b60d6ff3eac8c27281d1f23b1dfada4969fd6254f9951dfd83f9cf"
+      sha256 "cdaa13dd96187622ef5bee52867c46d4cf10765963423dc8e867c7c4decccf4d"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/aquaproj/aqua/releases/download/v#{version}/aqua_linux_amd64.tar.gz"
-      sha256 "cb7780962ca651c4e025a027b7bfc82c010af25c5c150fe89ad72f4058d46540"
+      sha256 "7371b9785e07c429608a21e4d5b17dafe6780dabe306ec9f4be842ea754de48a"
     end
     on_arm do
       url "https://github.com/aquaproj/aqua/releases/download/v#{version}/aqua_linux_arm64.tar.gz"
-      sha256 "d65369f52c9a74a2b0c24f494bfbe639794f4aab7788274e04272740b2c27a16"
+      sha256 "283e0e274af47ff1d4d660a19e8084ae4b6aca23d901e95728a68a63dfb98c87"
     end
   end
 
